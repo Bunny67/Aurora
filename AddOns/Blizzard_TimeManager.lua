da825d3dceee0f364b204c669504fc371bce9e58
Bunny67 <zolov67@yandex.ru>
2013-12-18 19:15:14 +0400

    Переписал Blizzard_TimeManager

--- AddOns/Blizzard_TimeManager.lua
+++ AddOns/Blizzard_TimeManager.lua
@@ -1,48 +1,42 @@
 local F, C = unpack(select(2, ...))
--- ВРЕМЯ
-C.modules["Blizzard_TimeManager"] = function()
-	if AuroraConfig.TimeManager then
-	-- [[ Чистка текстур ]]
-		local AllTimeManagerStripTextures = {'TimeManagerFrame', 'TimeManagerStopwatchFrame', 'StopwatchFrame', 'StopwatchTabFrame'}
-		for i = 1, #AllTimeManagerStripTextures do
-			F.StripTextures(_G[AllTimeManagerStripTextures[i]], true)
-		end
-	-- [[ Выпадающая кнопка ]]
-		local AllTimeManagerDropDowns = {'TimeManagerAlarmHourDropDown', 'TimeManagerAlarmMinuteDropDown', 'TimeManagerAlarmAMPMDropDown'}
-		for i = 1, #AllTimeManagerDropDowns do
-			F.ReskinDropDown(_G[AllTimeManagerDropDowns[i]])
-		end
-		
-		TimeManagerAlarmHourDropDown:SetWidth(80)
-		TimeManagerAlarmMinuteDropDown:SetWidth(80)
-		TimeManagerAlarmAMPMDropDown:SetWidth(90)
-	-- [[ Поля ввода ]]
-		local AllTimeManagerInputs = {'TimeManagerAlarmMessageEditBox'}
-		for i = 1, #AllTimeManagerInputs do
-			F.ReskinInput(_G[AllTimeManagerInputs[i]])
-		end
-	-- [[ Флажки ]]
-		local AllTimeManagerCheckBoxes = {'TimeManagerMilitaryTimeCheck', 'TimeManagerLocalTimeCheck'}
-		for i = 1, #AllTimeManagerCheckBoxes do
-			F.ReskinCheck(_G[AllTimeManagerCheckBoxes[i]])
-		end
-	-- [[ Кнопки ]]
-		local AllTimeManagerButtons = {'TimeManagerAlarmEnabledButton'}
-		for i = 1, #AllTimeManagerButtons do
-			F.Reskin(_G[AllTimeManagerButtons[i]])
-		end
 
-		F.CreateBD(TimeManagerStopwatchCheck, .25)
-		F.StyleButton(TimeManagerStopwatchCheck)
-		TimeManagerStopwatchCheck:GetNormalTexture():SetTexCoord(.08, .92, .08, .92)
-		TimeManagerStopwatchCheck:GetNormalTexture():ClearAllPoints()
-		TimeManagerStopwatchCheck:GetNormalTexture():SetPoint("TOPLEFT", 1, -1)
-		TimeManagerStopwatchCheck:GetNormalTexture():SetPoint("BOTTOMRIGHT", -1, 1)
-		
-		F.SetBD(StopwatchFrame, 0, -16, 0, 2)
-		F.ReskinClose(StopwatchCloseButton, 'TOPRIGHT', StopwatchFrame, 'TOPRIGHT', 0, 6)
-		
-		F.SetBD(TimeManagerFrame, 14, -11, -49, 9)
-		F.ReskinClose(TimeManagerCloseButton, 'TOPRIGHT', TimeManagerFrame, 'TOPRIGHT', -53, -15)
-	end
+local _G = getfenv(0);
+
+C.modules['Blizzard_TimeManager'] = function()
+	F.StripTextures(_G['TimeManagerFrame']);
+	F.SetBD(_G['TimeManagerFrame'], 14, -11, -49, 9);
+	
+	F.ReskinClose(_G['TimeManagerCloseButton'], 'TOPRIGHT', TimeManagerFrame, 'TOPRIGHT', -53, -15);
+	
+	F.StripTextures(_G['TimeManagerStopwatchFrame']);
+	
+	F.CreateBD(_G['TimeManagerStopwatchCheck'], .25);
+	F.StyleButton(_G['TimeManagerStopwatchCheck'], nil, true);
+	
+	_G['TimeManagerStopwatchCheck']:GetNormalTexture():SetPoint('TOPLEFT', 1, -1);
+	_G['TimeManagerStopwatchCheck']:GetNormalTexture():SetPoint('BOTTOMRIGHT', -1, 1);
+	_G['TimeManagerStopwatchCheck']:GetNormalTexture():SetTexCoord(unpack(F.TexCoords));
+	
+	_G['TimeManagerAlarmHourDropDown']:SetWidth(80);
+	F.ReskinDropDown(_G['TimeManagerAlarmHourDropDown']);
+	
+	_G['TimeManagerAlarmMinuteDropDown']:SetWidth(80);
+	F.ReskinDropDown(_G['TimeManagerAlarmMinuteDropDown']);
+	
+	_G['TimeManagerAlarmAMPMDropDown']:SetWidth(90);
+	F.ReskinDropDown(_G['TimeManagerAlarmAMPMDropDown']);
+	
+	F.ReskinInput(_G['TimeManagerAlarmMessageEditBox']);
+	
+	F.StripTextures(_G['TimeManagerMilitaryTimeCheck']);
+	F.StripTextures(_G['TimeManagerLocalTimeCheck']);
+	
+	F.Reskin(_G['TimeManagerAlarmEnabledButton'], nil, true);
+	
+	F.StripTextures(_G['StopwatchFrame']);
+	F.SetBD(_G['StopwatchFrame'], 0, -16, 0, 2);
+	
+	F.ReskinClose(_G['StopwatchCloseButton'], 'TOPRIGHT', StopwatchFrame, 'TOPRIGHT', 0, 6);
+	
+	F.StripTextures(_G['StopwatchTabFrame']);
 end
