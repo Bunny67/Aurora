93e02b1e4afe20f8abe17c31cf2f5af37892136a
Bunny67 <zolov67@yandex.ru>
2013-12-18 15:39:16 +0400

    Переписал Blizzard_GlyphUI

--- AddOns/Blizzard_GlyphUI.lua
+++ AddOns/Blizzard_GlyphUI.lua
@@ -1,21 +1,10 @@
-local F, C = unpack(select(2, ...))
--- СИМВОЛЫ
-C.modules["Blizzard_GlyphUI"] = function()
-	if AuroraConfig.GlyphUI then
-	-- [[ Чистка текстур ]]
-		local AllGlyphUIStripTextures = {'GlyphFrame'}
-		for i = 1, #AllGlyphUIStripTextures do
-		local AllGlyphUIStripTexture = _G[AllGlyphUIStripTextures[i]]
-			if AllGlyphUIStripTexture then
-				F.StripTextures(AllGlyphUIStripTexture, true)
-			else
-				print("Ошибка Aurora: "..AllGlyphUIStripTextures[i].." не был найден.")
-			end
-		end
-		
-		local bg = CreateFrame("Frame", nil, GlyphFrame)
-		bg:SetPoint("TOPLEFT", 12, -14)
-		bg:SetPoint("BOTTOMRIGHT", -33, 78)
-		F.CreateBG(bg)
-	end
+local F, C = unpack(select(2, ...));
+
+C.modules['Blizzard_GlyphUI'] = function()
+	F.StripTextures(_G['GlyphFrame'], true);
+	
+	local BG = CreateFrame('Frame', nil, GlyphFrame);
+	BG:SetPoint('TOPLEFT', 12, -14);
+	BG:SetPoint('BOTTOMRIGHT', -33, 78);
+	F.CreateBG(BG);
 end
