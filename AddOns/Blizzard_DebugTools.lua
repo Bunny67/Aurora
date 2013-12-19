27a78fb2f7a180ed68cbce7ca811791b930fce76
Bunny67 <zolov67@yandex.ru>
2013-12-19 09:52:07 +0400

    Обновил Blizzard_DebugTools

--- AddOns/Blizzard_DebugTools.lua
+++ AddOns/Blizzard_DebugTools.lua
@@ -4,27 +4,27 @@ local _G = getfenv(0);
 
 C.modules['Blizzard_DebugTools'] = function()
 	local Texures = { 'TopLeft', 'TopRight', 'Top', 'BottomLeft', 'BottomRight', 'Bottom', 'Left', 'Right', 'TitleBG', 'DialogBG' };
-	for i=1, #Texures do
+	for i = 1, #Texures do
 		_G['ScriptErrorsFrame'..Texures[i]]:SetTexture(nil);
 		_G['EventTraceFrame'..Texures[i]]:SetTexture(nil);
 	end
 	-- ScriptErrors
-	F.CreateBD(_G['ScriptErrorsFrame']);
+	F.CreateBD(ScriptErrorsFrame);
 	
-	F.ReskinClose(_G['ScriptErrorsFrameClose']);
+	F.ReskinClose(ScriptErrorsFrameClose);
 	
-	F.ReskinScroll(_G['ScriptErrorsFrameScrollFrameScrollBar']);
+	F.ReskinScroll(ScriptErrorsFrameScrollFrameScrollBar);
 	
-	for i=1, _G['ScriptErrorsFrame']:GetNumChildren() do
+	for i = 1, _G['ScriptErrorsFrame']:GetNumChildren() do
 		local Child = select(i, _G['ScriptErrorsFrame']:GetChildren());
 		if Child:GetObjectType() == 'Button' and not Child:GetName() then
 			F.Reskin(Child);
 		end
 	end
 	-- EventTrace
-	F.CreateBD(_G['EventTraceFrame']);
+	F.CreateBD(EventTraceFrame);
 	
-	F.ReskinClose(_G['EventTraceFrameCloseButton']);
+	F.ReskinClose(EventTraceFrameCloseButton);
 	-- FrameStack
-	F.CreateBD(_G['FrameStackTooltip']);
+	F.CreateBD(FrameStackTooltip);
 end
