aa3a586d88856cd834c233341c87214245914458
Bunny67 <zolov67@yandex.ru>
2013-12-19 09:49:33 +0400

    Обновил Blizzard_BindingUI

--- AddOns/Blizzard_BindingUI.lua
+++ AddOns/Blizzard_BindingUI.lua
@@ -1,25 +1,25 @@
-local F, C = unpack(select(2, ...))
+local F, C = unpack(select(2, ...));
 
 local _G = getfenv(0);
 
-C.modules["Blizzard_BindingUI"] = function()
-	F.StripTextures(_G['KeyBindingFrame']);
-	F.SetBD(_G['KeyBindingFrame'], 2, 0, -42, 12);
+C.modules['Blizzard_BindingUI'] = function()
+	F.StripTextures(KeyBindingFrame);
+	F.SetBD(KeyBindingFrame, 2, 0, -42, 12);
 	
-	F.ReskinCheck(_G['KeyBindingFrameCharacterButton']);
+	F.ReskinCheck(KeyBindingFrameCharacterButton);
 	
 	for i = 1, KEY_BINDINGS_DISPLAYED do
-		local Key1Button = _G["KeyBindingFrameBinding"..i.."Key1Button"];
-		local Key2Button = _G["KeyBindingFrameBinding"..i.."Key2Button"];
+		local Key1Button = _G['KeyBindingFrameBinding'..i..'Key1Button'];
+		local Key2Button = _G['KeyBindingFrameBinding'..i..'Key2Button'];
 		
 		F.Reskin(Key1Button, nil, true);
 		F.Reskin(Key2Button, nil, true);
 	end
 	
-	F.ReskinScroll(_G['KeyBindingFrameScrollFrameScrollBar']);
+	F.ReskinScroll(KeyBindingFrameScrollFrameScrollBar);
 	
-	F.Reskin(_G['KeyBindingFrameDefaultButton']);
-	F.Reskin(_G['KeyBindingFrameUnbindButton']);
-	F.Reskin(_G['KeyBindingFrameOkayButton']);
-	F.Reskin(_G['KeyBindingFrameCancelButton']);
+	F.Reskin(KeyBindingFrameDefaultButton);
+	F.Reskin(KeyBindingFrameUnbindButton);
+	F.Reskin(KeyBindingFrameOkayButton);
+	F.Reskin(KeyBindingFrameCancelButton);
 end
