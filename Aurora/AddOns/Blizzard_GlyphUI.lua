a673206ec49a3ab62dac92458718604a6eeb5967
Bunny67 <zolov67@yandex.ru>
2013-12-31 12:40:36 +0400

    Переписал Blizzard_GlyphUI

--- Aurora/AddOns/Blizzard_GlyphUI.lua
+++ Aurora/AddOns/Blizzard_GlyphUI.lua
@@ -3,10 +3,34 @@ local F, C = unpack(select(2, ...));
 local _G = getfenv(0);
 
 C.modules['Blizzard_GlyphUI'] = function()
+	local r, g, b = C.r, C.g, C.b;
+	
 	F.StripTextures(GlyphFrame, true);
 	
-	local BG = CreateFrame('Frame', nil, GlyphFrame);
-	BG:SetPoint('TOPLEFT', 12, -14);
-	BG:SetPoint('BOTTOMRIGHT', -33, 78);
-	F.CreateBG(BG);
+	hooksecurefunc('GlyphFrameGlyph_UpdateSlot', function(self)
+		self.glyph:SetTexCoord(unpack(F.TexCoords));
+		self.setting:Hide();
+		self.highlight:SetTexture('');
+		self.background:Hide();
+		self.ring:Hide();
+		
+		if not self.bg then
+			self.bg = CreateFrame('Frame', nil, self);
+			self.bg:SetPoint('TOPLEFT', self.glyph, -1, 1);
+			self.bg:SetPoint('BOTTOMRIGHT', self.glyph, 1, -1);
+			self.bg:SetFrameLevel(self:GetFrameLevel() - 1);
+			
+			F.CreateBD(self.bg, .25);
+		end
+	end);
+	
+	hooksecurefunc('GlyphFrameGlyph_OnUpdate', function(self)
+		local id = self:GetID();
+		
+		if GlyphMatchesSocket(id) then
+			self.bg:SetBackdropBorderColor(r, g, b);
+		else
+			self.bg:SetBackdropBorderColor(0, 0, 0);
+		end
+	end);
 end
