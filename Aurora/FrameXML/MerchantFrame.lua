aa80d133ddf696f4a5854f9068a86fbc62335121
Bunny67 <zolov67@yandex.ru>
2014-01-08 21:28:30 +0400

    Переписал MerchantFrame

--- Aurora/FrameXML/MerchantFrame.lua
+++ Aurora/FrameXML/MerchantFrame.lua
@@ -3,69 +3,66 @@ local F, C = unpack(select(2, ...));
 local _G = getfenv(0);
 
 tinsert(C.modules['Aurora'], function()
-	F.StripTextures(MerchantFrame, true);
-	F.SetBD(MerchantFrame, 10, -12, -29, 61);
+	F.StripTextures(MerchantFrame);
+	F.SetBD(MerchantFrame, 10, -12, -33, 60);
 	
-	F.ReskinClose(MerchantFrameCloseButton, 'TOPRIGHT', MerchantFrame, 'TOPRIGHT', -33, -16);
+	MerchantFramePortrait:Hide();
+	
+	for i = 1, BUYBACK_ITEMS_PER_PAGE do
+		local Item = _G['MerchantItem'..i];
+		local ItemButton = _G['MerchantItem'..i..'ItemButton'];
+		local ItemButtonIconTexture = _G['MerchantItem'..i..'ItemButtonIconTexture'];
+		
+		F.StripTextures(Item, true);
+		F.CreateBD(Item, .25);
+		
+		F.StripTextures(ItemButton);
+		F.StyleButton(ItemButton);
+		F.CreateBD(ItemButton);
+		ItemButton:SetSize(44, 44);
+		
+		ItemButtonIconTexture:SetTexCoord(unpack(F.TexCoords));
+		ItemButtonIconTexture:SetPoint('TOPLEFT', 1, -1);
+		ItemButtonIconTexture:SetPoint('BOTTOMRIGHT', -1, 1);
+		
+		for j = 1, 3 do
+			local AltCurrencyTexture = _G['MerchantItem'..i..'AltCurrencyFrameItem'..j..'Texture'];
+			
+			AltCurrencyTexture:SetTexCoord(unpack(F.TexCoords));
+		end
+	end
+	
+	F.StyleButton(MerchantRepairAllButton);
+	F.CreateBD(MerchantRepairAllButton);
+	MerchantRepairAllIcon:SetTexCoord(0.31375, 0.53, 0.06, 0.52);
+	
+	F.StyleButton(MerchantRepairItemButton);
+	F.CreateBD(MerchantRepairItemButton);
+	local ItemButton = MerchantRepairItemButton:GetRegions();
+	ItemButton:SetTexture('Interface\\Icons\\INV_Hammer_20');
+	ItemButton:SetTexCoord(unpack(F.TexCoords));
+
+	F.StyleButton(MerchantGuildBankRepairButton);
+	F.CreateBD(MerchantGuildBankRepairButton);
+	MerchantGuildBankRepairButtonIcon:SetTexCoord(0.595, 0.8075, 0.05, 0.52);
 	
 	F.StripTextures(MerchantBuyBackItem);
-
-	for i = 1, 2 do
-		F.ReskinTab(_G['MerchantFrameTab'..i])
-	end
-
-	for i = 1, 12 do
-		local Item = _G['MerchantItem'..i]
-		local ItemButton = _G['MerchantItem'..i..'ItemButton']
-		local ItemButtonIconTexture = _G['MerchantItem'..i..'ItemButtonIconTexture']
-		
-		F.StripTextures(Item, true)
-		F.CreateBD(Item, .25)
-		
-		F.StripTextures(ItemButton)
-		F.StyleButton(ItemButton)
-		F.CreateBD(ItemButton, .25)
-		ItemButton:SetSize(44, 44)
-		
-		ItemButtonIconTexture:SetTexCoord(unpack(F.TexCoords))
-		ItemButtonIconTexture:SetPoint('TOPLEFT', 1, -1)
-		ItemButtonIconTexture:SetPoint('BOTTOMRIGHT', -1, 1)
-		
-		_G['MerchantItem'..i..'MoneyFrame']:ClearAllPoints()
-		_G['MerchantItem'..i..'MoneyFrame']:SetPoint('BOTTOMLEFT', ItemButton, 'BOTTOMRIGHT', 2, 1)
-	end
+	F.StripTextures(MerchantBuyBackItemItemButton);
+	F.StyleButton(MerchantBuyBackItemItemButton);
+	F.CreateBD(MerchantBuyBackItemItemButton, 0);
+	MerchantBuyBackItemItemButtonIconTexture:SetTexCoord(unpack(F.TexCoords));
 	
-	F.StripTextures(MerchantBuyBackItemItemButton)
-	F.StyleButton(MerchantBuyBackItemItemButton)
-	F.CreateBD(MerchantBuyBackItemItemButton, .25)
-	MerchantBuyBackItemItemButtonIconTexture:SetTexCoord(unpack(F.TexCoords))
-	MerchantBuyBackItemItemButtonIconTexture:SetPoint('TOPLEFT', 1, -1)
-	MerchantBuyBackItemItemButtonIconTexture:SetPoint('BOTTOMRIGHT', -1, 1)
-	
-	F.StyleButton(MerchantRepairItemButton)
-	F.CreateBD(MerchantRepairItemButton, .25)
-	local ItemButton = MerchantRepairItemButton:GetRegions()
-	ItemButton:SetTexture('Interface\\Icons\\INV_Hammer_20')
-	ItemButton:SetTexCoord(unpack(F.TexCoords))
-	
-	F.StyleButton(MerchantGuildBankRepairButton)
-	F.CreateBD(MerchantGuildBankRepairButton, .25)
-	MerchantGuildBankRepairButtonIcon:SetTexCoord(0.61, 0.82, 0.1, 0.52)
-	MerchantGuildBankRepairButtonIcon:SetPoint('TOPLEFT', 1, -1)
-	MerchantGuildBankRepairButtonIcon:SetPoint('BOTTOMRIGHT', -1, 1)
-	
-	F.StyleButton(MerchantRepairAllButton)
-	F.CreateBD(MerchantRepairAllButton, .25)
-	MerchantRepairAllIcon:SetTexCoord(0.34, 0.1, 0.34, 0.535, 0.535, 0.1, 0.535, 0.535)
-	MerchantRepairAllIcon:SetPoint('TOPLEFT', 1, -1)
-	MerchantRepairAllIcon:SetPoint('BOTTOMRIGHT', -1, 1)
-	
-	MerchantPrevPageButton:GetRegions():Hide();
-	MerchantNextPageButton:GetRegions():Hide();
 	select(2, MerchantPrevPageButton:GetRegions()):Hide();
-	select(2, MerchantNextPageButton:GetRegions()):Hide();
 	F.ReskinArrow(MerchantPrevPageButton, 'left');
-	MerchantPrevPageButton:SetPoint('CENTER', MerchantFrame, 'BOTTOMLEFT', 33, 156);
+	
+	select(2, MerchantNextPageButton:GetRegions()):Hide();
 	F.ReskinArrow(MerchantNextPageButton, 'right');
-	MerchantNextPageButton:SetPoint('CENTER', MerchantFrame, 'BOTTOMLEFT', 332, 156);
-end)
+	
+	F.ReskinClose(MerchantFrameCloseButton, 'TOPRIGHT', MerchantFrame, 'TOPRIGHT', -38, -16);
+	
+	for i = 1, 2 do
+		local Tab = _G['MerchantFrameTab'..i];
+		
+		F.ReskinTab(Tab);
+	end
+end);
