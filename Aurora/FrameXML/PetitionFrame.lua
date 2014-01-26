5a265175db282dcf945bfcc2f2726ed97415a0f3
Bunny67 <zolov67@yandex.ru>
2014-01-27 02:57:51 +0400

    Добавил PetitionFrame

---
+++ Aurora/FrameXML/PetitionFrame.lua
@@ -0,0 +1,33 @@
+local F, C = unpack(select(2, ...));
+
+local _G = getfenv(0);
+
+tinsert(C.modules['Aurora'], function()
+	F.StripTextures(PetitionFrame, true);
+	F.SetBD(PetitionFrame, 15, -19, -30, 68);
+	
+	PetitionFrameCharterTitle:SetTextColor(1, 1, 1);
+	PetitionFrameCharterTitle:SetShadowColor(0, 0, 0);
+	PetitionFrameCharterName:SetTextColor(1, 1, 1);
+	PetitionFrameMasterTitle:SetTextColor(1, 1, 1);
+	PetitionFrameMasterTitle:SetShadowColor(0, 0, 0);
+	PetitionFrameMasterName:SetTextColor(1, 1, 1);
+	PetitionFrameMemberTitle:SetTextColor(1, 1, 1);
+	PetitionFrameMemberTitle:SetShadowColor(0, 0, 0);
+	
+	for i = 1, 9 do
+		local Name = _G['PetitionFrameMemberName'..i];
+		
+		Name:SetTextColor(1, 1, 1);
+	end
+	
+	PetitionFrameInstructions:SetTextColor(1, 1, 1);
+	
+	F.Reskin(PetitionFrameCancelButton);
+	F.Reskin(PetitionFrameSignButton);
+	F.Reskin(PetitionFrameRequestButton);
+	
+	F.ReskinClose(PetitionFrameCloseButton, 'TOPRIGHT', PetitionFrame, 'TOPRIGHT', -34, -23);
+	
+	F.Reskin(PetitionFrameRenameButton);
+end);
