3d411d5ec10690eb284866b90cce2a1f2a0217ad
Bunny67 <zolov67@yandex.ru>
2014-01-30 18:24:21 +0400

    Переписал ArenaFrame

--- Aurora/FrameXML/ArenaFrame.lua
+++ Aurora/FrameXML/ArenaFrame.lua
@@ -1,18 +1,13 @@
 local F, C = unpack(select(2, ...))
 
 tinsert(C.modules['Aurora'], function()
-	local AllArenaFrameStripTextures = {'ArenaFrame'}
-	for i = 1, #AllArenaFrameStripTextures do
-		F.StripTextures(_G[AllArenaFrameStripTextures[i]], true)
-	end
-
-	local AllArenaFrameButtons = {'ArenaFrameGroupJoinButton', 'ArenaFrameJoinButton', 'ArenaFrameCancelButton'}
-	for i = 1, #AllArenaFrameButtons do
-		F.Reskin(_G[AllArenaFrameButtons[i]])
-	end
-
-	ArenaFrameZoneDescription:SetTextColor(1, 1, 1)
+	F.SetBD(ArenaFrame, 11, -12, -34, 74);
 	
-	F.SetBD(ArenaFrame, 10, -12, -31, 74)
-	F.ReskinClose(ArenaFrameCloseButton, 'TOPRIGHT', ArenaFrame, 'TOPRIGHT', -35, -16)
+	ArenaFrameZoneDescription:SetTextColor(1, 1, 1);
+	
+	F.Reskin(ArenaFrameCancelButton);
+	F.Reskin(ArenaFrameJoinButton);
+	F.Reskin(ArenaFrameGroupJoinButton);
+	
+	F.ReskinClose(ArenaFrameCloseButton, 'TOPRIGHT', ArenaFrame, 'TOPRIGHT', -38, -16);
 end)
