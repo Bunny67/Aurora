8d97ca969c9829bb52543ebcee54e10e70339703
Bunny67 <zolov67@yandex.ru>
2014-04-17 23:25:45 +0400

    Поправил Blizzard_TimeManager

--- Aurora/AddOns/Blizzard_TimeManager.lua
+++ Aurora/AddOns/Blizzard_TimeManager.lua
@@ -1,43 +1,50 @@
 local F, C = unpack(select(2, ...));
 
-local _G = getfenv(0);
 local unpack = unpack;
 
+local TexCoords = F.TexCoords;
+local Hoop = F.dummy;
+
 C.modules['Blizzard_TimeManager'] = function()
-	F.StripTextures(TimeManagerFrame);
+	-- TimeManagerFrame;
 	F.SetBD(TimeManagerFrame, 14, -11, -49, 9);
 	
 	F.ReskinClose(TimeManagerCloseButton, 'TOPRIGHT', TimeManagerFrame, 'TOPRIGHT', -53, -15);
 	
-	F.StripTextures(TimeManagerStopwatchFrame);
+	TimeManagerStopwatchFrameBackground:Hide();
 	
-	F.CreateBD(TimeManagerStopwatchCheck, .25);
+	F.CreateBD(TimeManagerStopwatchCheck);
 	F.StyleButton(TimeManagerStopwatchCheck, nil, true);
 	
-	_G['TimeManagerStopwatchCheck']:GetNormalTexture():SetPoint('TOPLEFT', 1, -1);
-	_G['TimeManagerStopwatchCheck']:GetNormalTexture():SetPoint('BOTTOMRIGHT', -1, 1);
-	_G['TimeManagerStopwatchCheck']:GetNormalTexture():SetTexCoord(unpack(F.TexCoords));
+	TimeManagerStopwatchCheck:GetNormalTexture():SetPoint('TOPLEFT', 1, -1);
+	TimeManagerStopwatchCheck:GetNormalTexture():SetPoint('BOTTOMRIGHT', -1, 1);
+	TimeManagerStopwatchCheck:GetNormalTexture():SetTexCoord(unpack(TexCoords));
 	
-	_G['TimeManagerAlarmHourDropDown']:SetWidth(80);
+	TimeManagerAlarmHourDropDown:SetWidth(80);
 	F.ReskinDropDown(TimeManagerAlarmHourDropDown);
-	
-	_G['TimeManagerAlarmMinuteDropDown']:SetWidth(80);
+	TimeManagerAlarmMinuteDropDown:SetWidth(80);
 	F.ReskinDropDown(TimeManagerAlarmMinuteDropDown);
-	
-	_G['TimeManagerAlarmAMPMDropDown']:SetWidth(90);
+	TimeManagerAlarmAMPMDropDown:SetWidth(90);
 	F.ReskinDropDown(TimeManagerAlarmAMPMDropDown);
 	
 	F.ReskinInput(TimeManagerAlarmMessageEditBox);
 	
+	TimeManagerAlarmEnabledButton:SetNormalTexture('');
+	TimeManagerAlarmEnabledButton.SetNormalTexture = Hoop;
+	TimeManagerAlarmEnabledButton:SetPushedTexture('');
+	TimeManagerAlarmEnabledButton.SetPushedTexture = Hoop;
+	F.Reskin(TimeManagerAlarmEnabledButton);
+	
 	F.ReskinCheck(TimeManagerMilitaryTimeCheck);
 	F.ReskinCheck(TimeManagerLocalTimeCheck);
-	
-	F.Reskin(TimeManagerAlarmEnabledButton, nil, true);
-	
-	F.StripTextures(StopwatchFrame);
+	-- TimeManagerClockButton;
+	-- nil;
+	-- StopwatchFrame;
 	F.SetBD(StopwatchFrame, 0, -16, 0, 2);
 	
-	F.ReskinClose(StopwatchCloseButton, 'TOPRIGHT', StopwatchFrame, 'TOPRIGHT', 0, 6);
+	StopwatchTabFrameLeft:Hide();
+	StopwatchTabFrameMiddle:Hide();
+	StopwatchTabFrameRight:Hide();
 	
-	F.StripTextures(StopwatchTabFrame);
+	F.ReskinClose(StopwatchCloseButton, 'TOPRIGHT', StopwatchFrame, 'TOPRIGHT', 0, 4);
 end
