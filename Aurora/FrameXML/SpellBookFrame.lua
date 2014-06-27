ddd0ee7c5e609d48ca2cb8987fe94bebe8ff4a44
Bunny67 <zolov67@yandex.ru>
2014-06-27 11:18:16 +0400

    Поправил SpellBookFrame

--- Aurora/FrameXML/SpellBookFrame.lua
+++ Aurora/FrameXML/SpellBookFrame.lua
@@ -3,25 +3,31 @@ local F, C = unpack(select(2, ...));
 local _G = getfenv(0);
 local unpack = unpack;
 local select = select;
+local tinsert = table.insert;
+local strlen = string.len;
 
 local TexCoords = F.TexCoords;
 
 tinsert(C.Modules['Aurora'], function()
 	F.SetBD(SpellBookFrame, 13, -13, -31, 73);
-	SpellBookFrameIcon:Hide();
+	SpellBookFrameIcon:SetTexture(nil);
 	
 	for i = 2, 5 do
-		select(i, SpellBookFrame:GetRegions()):Hide();
+		select(i, SpellBookFrame:GetRegions()):SetTexture(nil);
 	end
 	
-	for i = 1, 3 do
-		local Tab = _G['SpellBookFrameTabButton'..i];
+	do
+		local Tab;
 		
-		Tab:SetNormalTexture('');
-		Tab:SetDisabledTexture('');
-		
-		F.ReskinTab(Tab);
-		Tab:SetHeight(32);
+		for i = 1, 3 do
+			Tab = _G['SpellBookFrameTabButton'..i];
+			
+			Tab:GetNormalTexture():SetTexture(nil);
+			Tab:GetDisabledTexture():SetTexture(nil);
+			
+			F.ReskinTab(Tab);
+			Tab:SetHeight(32);
+		end
 	end
 	
 	F.ReskinArrow(SpellBookPrevPageButton, 'Left');
@@ -30,33 +36,42 @@ tinsert(C.Modules['Aurora'], function()
 	F.ReskinClose(SpellBookCloseButton, 'TOPRIGHT', SpellBookFrame, 'TOPRIGHT', -35, -17);
 	
 	F.ReskinCheck(ShowAllSpellRanksCheckBox);
-
-	for i = 1, SPELLS_PER_PAGE do
-		local Button = _G['SpellButton'..i];
-		local IconTexture = _G['SpellButton'..i..'IconTexture'];
+	
+	do
+		local Button;
+		local ButtonIcon;
 		
-		Button:SetNormalTexture('');
-		
-		_G['SpellButton'..i..'Background']:Hide();
-		
-		if IconTexture then
-			Button.BG = F.CreateBG(Button);
+		for i = 1, SPELLS_PER_PAGE do
+			Button = _G['SpellButton'..i];
+			ButtonIcon = _G['SpellButton'..i..'IconTexture'];
 			
-			IconTexture:SetTexCoord(unpack(TexCoords));
+			Button:GetNormalTexture():SetTexture(nil);
+			
+			_G['SpellButton'..i..'Background']:SetTexture(nil);
+			
+			if ButtonIcon then
+				Button.BG = F.CreateBG(Button);
+				
+				ButtonIcon:SetTexCoord(unpack(TexCoords));
+			end
 		end
 	end
 	
-	for i = 1, MAX_SKILLLINE_TABS do
-		local Tab = _G['SpellBookSkillLineTab'..i];
+	do
+		local Tab;
 		
-		select(1, Tab:GetRegions()):Hide();
-		
-		F.StyleButton(Tab, nil, true);
-		F.CreateBD(Tab);
-		
-		Tab:GetNormalTexture():SetPoint('TOPLEFT', 1, -1);
-		Tab:GetNormalTexture():SetPoint('BOTTOMRIGHT', -1, 1);
-		Tab:GetNormalTexture():SetTexCoord(unpack(TexCoords));
+		for i = 1, MAX_SKILLLINE_TABS do
+			Tab = _G['SpellBookSkillLineTab'..i];
+			
+			select(1, Tab:GetRegions()):SetTexture(nil);
+			
+			F.StyleButton(Tab, nil, true);
+			F.CreateBD(Tab);
+			
+			Tab:GetNormalTexture():SetPoint('TOPLEFT', 1, -1);
+			Tab:GetNormalTexture():SetPoint('BOTTOMRIGHT', -1, 1);
+			Tab:GetNormalTexture():SetTexCoord(unpack(TexCoords));
+		end
 	end
 	
 	hooksecurefunc('SpellButton_UpdateButton', function(self)
@@ -75,6 +90,14 @@ tinsert(C.Modules['Aurora'], function()
 		Highlight:SetTexture(C.r, C.g, C.b, .25);
 		
 		if ( (SpellBookFrame.bookType ~= BOOKTYPE_PET) and (not DisplayID or DisplayID > (Offset + NumSpells)) ) then
+			self.BG:Hide();
+			
+			return;
+		end
+		
+		local Texture = GetSpellTexture(ID, SpellBookFrame.bookType);
+		
+		if ( not Texture or (strlen(Texture) == 0) ) then
 			self.BG:Hide();
 			
 			return;
