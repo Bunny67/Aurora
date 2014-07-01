36676ceb9b6c473fe3b397a64719daa99f2108c8
Bunny67 <zolov67@yandex.ru>
2014-07-01 18:37:36 +0400

    Поправил FriendsFrame

--- Aurora/FrameXML/FriendsFrame.lua
+++ Aurora/FrameXML/FriendsFrame.lua
@@ -77,8 +77,11 @@ tinsert(C.Modules['Aurora'], function()
 	WhoListScrollFrame:DisableDrawLayer('BACKGROUND');
 	F.ReskinScroll(WhoListScrollFrameScrollBar);
 	-- GuildFrame;
-	GuildFrameLFGFrame:DisableDrawLayer('BACKGROUND');
 	F.CreateBD(GuildFrameLFGFrame, .25);
+	
+	for i = 1, 3 do
+		select(i, GuildFrameLFGFrame:GetRegions()):SetTexture(nil);
+	end
 	
 	F.ReskinCheck(GuildFrameLFGButton);
 	
@@ -87,6 +90,15 @@ tinsert(C.Modules['Aurora'], function()
 		
 		for i = 1, 4 do
 			Header = _G['GuildFrameColumnHeader'..i];
+			
+			F.ReskinHeader(Header);
+		end
+	end
+	
+	do
+		local Header;
+		
+		for i = 1, 4 do
 			Header = _G['GuildFrameGuildStatusColumnHeader'..i];
 			
 			F.ReskinHeader(Header);
@@ -102,80 +114,104 @@ tinsert(C.Modules['Aurora'], function()
 	F.Reskin(GuildFrameAddMemberButton);
 	F.Reskin(GuildFrameGuildInformationButton);
 	-- GuildControlPopupFrame;
+	F.SetBD(GuildControlPopupFrame, 2, -6, -27, 27);
 	
-	
-	F.StripTextures(GuildMemberDetailFrame); -- Иформация о персонаже
-	F.CreateBD(GuildMemberDetailFrame);
-	
-	F.ReskinClose(GuildMemberDetailCloseButton);
-	
-	F.CreateBD(GuildMemberNoteBackground, .25);
-	
-	F.CreateBD(GuildMemberOfficerNoteBackground, .25);
-	
-	F.Reskin(GuildMemberRemoveButton);
-	F.Reskin(GuildMemberGroupInviteButton);
-	
-	F.StripTextures(GuildInfoFrame); -- Информация
-	F.SetBD(GuildInfoFrame, 5, -6, -2, 6);
-	
-	F.ReskinClose(GuildInfoCloseButton, 'TOPRIGHT', GuildInfoFrame, 'TOPRIGHT', -6, -10);
-	
-	F.CreateBD(GuildInfoTextBackground, .25);
-	
-	F.ReskinScroll(GuildInfoFrameScrollFrameScrollBar);
-	
-	F.Reskin(GuildInfoGuildEventButton);
-	F.Reskin(GuildInfoSaveButton);
-	F.Reskin(GuildInfoCancelButton);
-	
-	F.StripTextures(GuildEventLogFrame); -- Журнал
-	F.SetBD(GuildEventLogFrame, 5, -6, -2, 6);
-	
-	F.ReskinClose(GuildEventLogCloseButton, 'TOPRIGHT', GuildEventLogFrame, 'TOPRIGHT', -6, -10);
-	
-	F.CreateBD(GuildEventFrame, .25);
-	
-	F.ReskinScroll(GuildEventLogScrollFrameScrollBar);
-	
-	F.Reskin(GuildEventLogCancelButton);
-	
-	F.StripTextures(GuildControlPopupFrame); -- Управление
-	F.SetBD(GuildControlPopupFrame, 3, -5, -27, 27);
+	for i = 1, 6 do
+		select(i, GuildControlPopupFrame:GetRegions()):SetTexture(nil);
+	end
 	
 	F.ReskinDropDown(GuildControlPopupFrameDropDown);
-	--_G['GuildControlPopupFrameDropDownButton']:SetSize(16, 16);
 	
 	F.ReskinExpandOrCollapse(GuildControlPopupFrameAddRankButton);
+	F.ReskinExpandOrCollapse(GuildControlPopupFrameRemoveRankButton);
+	GuildControlPopupFrameRemoveRankButton.Plus:Hide();
+	GuildControlPopupFrameRemoveRankButton:SetPoint('LEFT', GuildControlPopupFrameAddRankButton, 'RIGHT', 1, 0);
 	
-	F.ReskinInput(GuildControlPopupFrameEditBox, 14);
-	
-	for i = 1, 17 do
-		local Checkbox = _G['GuildControlPopupFrameCheckbox'..i];
+	do
+		local Checkbox;
 		
-		if (Checkbox) then
-			F.ReskinCheck(Checkbox);
+		for i = 1, 17 do
+			Checkbox = _G['GuildControlPopupFrameCheckbox'..i];
+			
+			if ( Checkbox ) then
+				F.ReskinCheck(Checkbox);
+			end
 		end
 	end
 	
-	F.ReskinInput(GuildControlWithdrawGoldEditBox, 14);
+	F.ReskinInput(GuildControlWithdrawGoldEditBox, 16);
 	
-	for i = 1, MAX_GUILDBANK_TABS do
-		local Tab = _G['GuildBankTabPermissionsTab'..i];
-		
-		F.StripTextures(Tab);
-	end
+	F.Reskin(GuildControlPopupFrameCancelButton);
+	F.Reskin(GuildControlPopupAcceptButton);
 	
-	--_G['GuildControlPopupFrameTabPermissions'];
+	F.ReskinInput(GuildControlPopupFrameEditBox, 16);
+	
+	F.CreateBD(GuildControlPopupFrameTabPermissions, .25);
 	
 	F.ReskinCheck(GuildControlTabPermissionsViewTab);
 	F.ReskinCheck(GuildControlTabPermissionsDepositItems);
 	F.ReskinCheck(GuildControlTabPermissionsUpdateText);
 	
-	F.ReskinInput(GuildControlWithdrawItemsEditBox, 14);
+	F.ReskinInput(GuildControlWithdrawItemsEditBox, 16, 54);
 	
-	F.Reskin(GuildControlPopupAcceptButton);
-	F.Reskin(GuildControlPopupFrameCancelButton);
+	do
+		local Tab;
+		
+		for i = 1, MAX_GUILDBANK_TABS do
+			Tab = _G['GuildBankTabPermissionsTab'..i];
+			
+			Tab:DisableDrawLayer('BACKGROUND');
+			
+			Tab:SetHighlightTexture(nil);
+		end
+	end
+	-- GuildInfoFrame;
+	F.SetBD(GuildInfoFrame, 5, -6, -2, 6);
+	
+	GuildInfoCorner:SetTexture(nil);
+	
+	F.CreateBD(GuildInfoTextBackground, .25);
+	
+	F.ReskinScroll(GuildInfoFrameScrollFrameScrollBar);
+	
+	F.ReskinClose(GuildInfoCloseButton, 'TOPRIGHT', GuildInfoFrame, 'TOPRIGHT', -6, -10);
+	
+	F.Reskin(GuildInfoSaveButton);
+	F.Reskin(GuildInfoCancelButton);
+	GuildInfoCancelButton:SetPoint('LEFT', GuildInfoSaveButton, 'RIGHT', 1, 0);
+	F.Reskin(GuildInfoGuildEventButton);
+	
+	GuildInfoFrame:SetBackdrop(nil);
+	-- GuildMemberDetailFrame;
+	F.CreateBD(GuildMemberDetailFrame);
+	select(11, GuildMemberDetailFrame:GetRegions()):SetTexture(nil);
+	
+	GuildMemberDetailCorner:SetTexture(nil);
+	
+	F.ReskinClose(GuildMemberDetailCloseButton);
+	
+	F.Reskin(GuildMemberRemoveButton);
+	F.Reskin(GuildMemberGroupInviteButton);
+	
+	F.ReskinArrow(GuildFramePromoteButton, 'Up');
+	F.ReskinArrow(GuildFrameDemoteButton, 'Down');
+	GuildFrameDemoteButton:SetPoint('LEFT', GuildFramePromoteButton, 'RIGHT', 1, 0);
+	
+	F.CreateBD(GuildMemberNoteBackground, .25);
+	F.CreateBD(GuildMemberOfficerNoteBackground, .25);
+	-- GuildEventLogFrame;
+	F.SetBD(GuildEventLogFrame, 5, -6, -2, 6);
+	select(1, GuildEventLogFrame:GetRegions()):SetTexture(nil);
+	
+	F.CreateBD(GuildEventFrame, .25);
+	
+	F.ReskinScroll(GuildEventLogScrollFrameScrollBar);
+	
+	F.ReskinClose(GuildEventLogCloseButton, 'TOPRIGHT', GuildEventLogFrame, 'TOPRIGHT', -6, -10);
+	
+	F.Reskin(GuildEventLogCancelButton);
+	
+	GuildEventLogFrame:SetBackdrop(nil);
 	
 	do
 		local Tab;
