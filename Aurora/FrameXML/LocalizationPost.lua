fefffc579d6d534225002b038fb0b0863f3e2e7a
Bunny67 <zolov67@yandex.ru>
2015-02-26 17:46:28 +0300

    Исправлена ошибка на не русском клиенте

--- Aurora/FrameXML/LocalizationPost.lua
+++ Aurora/FrameXML/LocalizationPost.lua
@@ -6,23 +6,25 @@ local tinsert = table.insert;
 local F, C = unpack(select(2, ...));
 
 tinsert(C.Modules['Aurora'], function()
-	F:CreateBD(DeclensionFrame);
-	
-	do
-		local DeclensionEdit;
+	if(DeclensionFrame) then
+		F:CreateBD(DeclensionFrame);
 		
-		for i = 1, RUSSIAN_DECLENSION_PATTERNS do
-			DeclensionEdit = _G['DeclensionFrameDeclension'..i..'Edit'];
+		do
+			local DeclensionEdit;
 			
-			DeclensionEdit:SetBackdrop(nil);
-			
-			F:ReskinInput(DeclensionEdit);
+			for i = 1, RUSSIAN_DECLENSION_PATTERNS do
+				DeclensionEdit = _G['DeclensionFrameDeclension'..i..'Edit'];
+				
+				DeclensionEdit:SetBackdrop(nil);
+				
+				F:ReskinInput(DeclensionEdit);
+			end
 		end
+		
+		F:ReskinArrow(DeclensionFrameSetNext, 'Right');
+		F:ReskinArrow(DeclensionFrameSetPrev, 'Left');
+		
+		F:Reskin(DeclensionFrameOkayButton);
+		F:Reskin(DeclensionFrameCancelButton);
 	end
-	
-	F:ReskinArrow(DeclensionFrameSetNext, 'Right');
-	F:ReskinArrow(DeclensionFrameSetPrev, 'Left');
-	
-	F:Reskin(DeclensionFrameOkayButton);
-	F:Reskin(DeclensionFrameCancelButton);
 end);
